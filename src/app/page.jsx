9e1f19bda1bdd11ba8d37a6786e3e4c927d84eae
GrantLicata <grant.licata@gmail.com>
2023-09-20 22:48:39 -0700

    style adjustments

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,45 @@
+import Link from "next/link";
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Link href="/">
+        <h1>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
+          et nobis velit. Deserunt, debitis sed itaque, ea provident quis
+          mollitia recusandae adipisci voluptates necessitatibus eligendi
+          tempora laborum sit veritatis asperiores.
+        </h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus
+        obcaecati animi aperiam, sint consectetur explicabo unde repellat modi
+        quis ipsam mollitia perspiciatis numquam expedita quos, harum commodi
+        laboriosam rem dolorem. Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Possimus obcaecati animi aperiam, sint consectetur
+        explicabo unde repellat modi quis ipsam mollitia perspiciatis numquam
+        expedita quos, harum commodi laboriosam rem dolorem. Lorem ipsum dolor
+        sit amet, consectetur adipisicing elit. Possimus obcaecati animi
+        aperiam, sint consectetur explicabo unde repellat modi quis ipsam
+        mollitia perspiciatis numquam expedita quos, harum commodi laboriosam
+        rem dolorem. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Possimus obcaecati animi aperiam, sint consectetur explicabo unde
+        repellat modi quis ipsam mollitia perspiciatis numquam expedita quos,
+        harum commodi laboriosam rem dolorem. Lorem ipsum dolor sit amet,
+        consectetur adipisicing elit. Possimus obcaecati animi aperiam, sint
+        consectetur explicabo unde repellat modi quis ipsam mollitia
+        perspiciatis numquam expedita quos, harum commodi laboriosam rem
+        dolorem. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Possimus obcaecati animi aperiam, sint consectetur explicabo unde
+        repellat modi quis ipsam mollitia perspiciatis numquam expedita quos,
+        harum commodi laboriosam rem dolorem. Lorem ipsum dolor sit amet,
+        consectetur adipisicing elit. Possimus obcaecati animi aperiam, sint
+        consectetur explicabo unde repellat modi quis ipsam mollitia
+        perspiciatis numquam expedita quos, harum commodi laboriosam rem
+        dolorem. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Possimus obcaecati animi aperiam, sint consectetur explicabo unde
+        repellat modi quis ipsam mollitia perspiciatis numquam expedita quos,
+        harum commodi laboriosam rem dolorem.
+      </Link>
+    </div>
+  );
 }
