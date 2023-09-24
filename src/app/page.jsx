c31c952b5f728d28845670146d2d2b50b44b7b97
GrantLicata <grant.licata@gmail.com>
2023-09-24 16:20:45 -0700

    Additional components added for blog view

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,6 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
-import Footer from "@/components/footer/Footer";
-import Navbar from "@/components/navbar/Navbar";
 
 export default function Home() {
-  return (
-    <div>
-      <Navbar />
-      <Footer />
-    </div>
-  );
+  return <div></div>;
 }
