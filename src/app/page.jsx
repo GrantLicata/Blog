5c9f1d6cf2bc76688c50447616e86d5343cf805c
GrantLicata <grant.licata@gmail.com>
2023-09-24 16:10:19 -0700

    Navbar and footer styled

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,45 +1,13 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
+import Footer from "@/components/footer/Footer";
+import Navbar from "@/components/navbar/Navbar";
 
 export default function Home() {
   return (
     <div>
-      <Link href="/">
-        <h1>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
-          et nobis velit. Deserunt, debitis sed itaque, ea provident quis
-          mollitia recusandae adipisci voluptates necessitatibus eligendi
-          tempora laborum sit veritatis asperiores.
-        </h1>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus
-        obcaecati animi aperiam, sint consectetur explicabo unde repellat modi
-        quis ipsam mollitia perspiciatis numquam expedita quos, harum commodi
-        laboriosam rem dolorem. Lorem ipsum dolor sit amet, consectetur
-        adipisicing elit. Possimus obcaecati animi aperiam, sint consectetur
-        explicabo unde repellat modi quis ipsam mollitia perspiciatis numquam
-        expedita quos, harum commodi laboriosam rem dolorem. Lorem ipsum dolor
-        sit amet, consectetur adipisicing elit. Possimus obcaecati animi
-        aperiam, sint consectetur explicabo unde repellat modi quis ipsam
-        mollitia perspiciatis numquam expedita quos, harum commodi laboriosam
-        rem dolorem. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Possimus obcaecati animi aperiam, sint consectetur explicabo unde
-        repellat modi quis ipsam mollitia perspiciatis numquam expedita quos,
-        harum commodi laboriosam rem dolorem. Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Possimus obcaecati animi aperiam, sint
-        consectetur explicabo unde repellat modi quis ipsam mollitia
-        perspiciatis numquam expedita quos, harum commodi laboriosam rem
-        dolorem. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Possimus obcaecati animi aperiam, sint consectetur explicabo unde
-        repellat modi quis ipsam mollitia perspiciatis numquam expedita quos,
-        harum commodi laboriosam rem dolorem. Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit. Possimus obcaecati animi aperiam, sint
-        consectetur explicabo unde repellat modi quis ipsam mollitia
-        perspiciatis numquam expedita quos, harum commodi laboriosam rem
-        dolorem. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Possimus obcaecati animi aperiam, sint consectetur explicabo unde
-        repellat modi quis ipsam mollitia perspiciatis numquam expedita quos,
-        harum commodi laboriosam rem dolorem.
-      </Link>
+      <Navbar />
+      <Footer />
     </div>
   );
 }
