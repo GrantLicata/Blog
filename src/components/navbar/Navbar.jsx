c325b18c89ab817992347d7da7c881e6268257b3
GrantLicata <grant.licata@gmail.com>
2023-09-24 17:00:49 -0700

    Prepping the theme toggle

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import styles from "./navbar.module.css";
 import Image from "next/image";
 import Link from "next/link";
 // import AuthLinks from "../authLinks/AuthLinks";
-// import ThemeToggle from "../themeToggle/ThemeToggle";
+import ThemeToggle from "../themeToggle/ThemeToggle";
 
 const Navbar = () => {
   return (
