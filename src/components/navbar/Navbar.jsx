e23156cb6db5bc4dc7e11e57b57ac650cf6a07c2
GrantLicata <grant.licata@gmail.com>
2023-09-24 17:06:17 -0700

    Auth links component built

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./navbar.module.css";
 import Image from "next/image";
 import Link from "next/link";
-// import AuthLinks from "../authLinks/AuthLinks";
+import AuthLinks from "../authLinks/AuthLinks";
 import ThemeToggle from "../themeToggle/ThemeToggle";
 
 const Navbar = () => {
@@ -26,7 +26,7 @@ const Navbar = () => {
         <Link href="/" className={styles.link}>
           About
         </Link>
-        {/* <AuthLinks /> */}
+        <AuthLinks />
       </div>
     </div>
   );
