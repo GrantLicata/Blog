1a7b9fc755b7dff1c0b84bf547bf578b767aec7a
GrantLicata <grant.licata@gmail.com>
2023-09-25 19:31:39 -0700

    Footer component built

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import styles from "./footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div className={styles.container}>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <p className={styles.desc}>©2023 Grant Licata, All rights reserved.</p>
+    </div>
+  );
 };
 
 export default Footer;
